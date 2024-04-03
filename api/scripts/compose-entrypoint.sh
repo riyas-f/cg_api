baea6e0ac49936b199be78c6efa895c54bca3542
= <=>
2024-04-03 18:46:43 +0700

    update api service entrypoint

--- api/scripts/compose-entrypoint.sh
+++ api/scripts/compose-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 # export steam key file content to env var 
 export DB_PASSWORD=$(cat $DB_PASSWORD_FILE)
+
+# export related stuff
+if [[ -n $HASH_SECRET_KEY_FILE]]; then 
+    export HASH_SECRET_KEY=$(cat $HASH_SECRET_KEY_FILE)
+elif [[ -n $JWT_SECRET_KEY_FILE]]; then 
+    export JWT_SECRET_KEY=$(cat $HASH_SECRET_KEY_FILE)
+fi
+
 ./app
