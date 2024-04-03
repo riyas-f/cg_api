2da1869a6562314b3b9f9de574c91f825c46aea7
= <=>
2024-04-03 09:02:54 +0700

    update cors impl

--- api/http/middleware/middleware.go
+++ api/http/middleware/middleware.go
@@ -2,17 +2,14 @@ package middleware
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
-
-	"github.com/rs/cors"
 )
 
 type ContextKey string
 
 type Middleware func(next http.Handler, db *sql.DB, conf interface{}) http.Handler
 
-func UseMiddleware(db *sql.DB, conf interface{}, handler http.Handler, corsOptions *cors.Options, middlewares ...Middleware) http.Handler {
+func UseMiddleware(db *sql.DB, conf interface{}, handler http.Handler, middlewares ...Middleware) http.Handler {
 	chained := handler
 
 	for i := len(middlewares) - 1; i > -1; i-- {
@@ -20,10 +17,10 @@ func UseMiddleware(db *sql.DB, conf interface{}, handler http.Handler, corsOptio
 	}
 
 	// append cors middleware into the chain
-	if corsOptions != nil {
-		fmt.Println(*corsOptions)
-		chained = cors.New(*corsOptions).Handler(chained)
-	}
+	// if corsOptions != nil {
+	// 	fmt.Println(*corsOptions)
+	// 	chained = cors.New(*corsOptions).Handler(chained)
+	// }
 
 	return chained
 }
