f7e381c3f57f9a37b565f9911d358dc3a9474278
= <=>
2024-04-04 21:24:51 +0700

    update cert-manager init scripts

--- cert-manager/scripts/init_key_cert.sh
+++ cert-manager/scripts/init_key_cert.sh
@@ -12,13 +12,17 @@ export COMMON_NAME="cert-manager.aditya.com"
 export ISSUER="/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${ORG_UNIT}/CN=${COMMON_NAME}"
 # export ABSOLUTE_CERT_PATH="$(pwd)/${CERT_FILE_PATH}"
 
-echo "$(openssl rand -base64 32)" | tr -d '\n' > /tmp/passphrase
-echo "$(cat /tmp/passphrase)"
+# echo "$(openssl rand -base64 32)" | tr -d '\n' > /tmp/passphrase
+# echo "$(cat /tmp/passphrase)"
 
-openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
-openssl req -new -x509 -key private_key.pem -out $CERT_FILE_PATH -days 365 -subj "${ISSUER}"
-openssl pkcs8 -topk8 -inform PEM -outform PEM -in private_key.pem -out $PRIVATE_KEY_PATH -passout file:/tmp/passphrase
+# openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
+# openssl req -new -x509 -key private_key.pem -out $CERT_FILE_PATH -days 365 -subj "${ISSUER}"
+# openssl pkcs8 -topk8 -inform PEM -outform PEM -in private_key.pem -out $PRIVATE_KEY_PATH -passout file:/tmp/passphrase
 
 
+# get root ca cert from google bucket 
+curl ${GCP_CERT_FILE_BUCKET_URL} -o $CERT_FILE_PATH
+gcloud secrets versions access latest --secret=${GCP_PRIVATE_KEY_SECRET_NAME} > $PRIVATE_KEY_PATH
+gcloud secrets versions access latest --secret=${GCP_PRIVATE_KEY_PASSPHRASE_SECRET_NAM} > /tmp/passphrase
 
-rm private_key.pem
+# rm private_key.pem
